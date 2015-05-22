8766d53d3bd8eb8e84334e669d5fed9526be3b6a
Hervé Darritchon <herve.darritchon@gmail.com>
2015-05-22 10:24:20 +0200

    Evolution: Add some tests to validate FizzBuzz App.

--- src/test/scala/fr/hervedarritchon/scala/kata/fizzbuzz/FizzBuzzSpec.scala
+++ src/test/scala/fr/hervedarritchon/scala/kata/fizzbuzz/FizzBuzzSpec.scala
@@ -7,7 +7,37 @@ import org.scalatest.{Matchers, FlatSpec}
  *
  */
 class FizzBuzzSpec extends FlatSpec with Matchers {
+
   "FizzBuzzSpec" should "have tests" in {
     true should ===(true)
   }
+
+  "A non special number" should "return itself" in {
+    "1" should === (FizzBuzz.start(1))
+  }
+
+  "A 3" should "return Fizz" in {
+    "Fizz" should === (FizzBuzz.start(3))
+  }
+
+  "A 5" should "return Buzz" in {
+    "Buzz" should === (FizzBuzz.start(5))
+  }
+
+  "A multiple of 3" should "return Fizz" in {
+    "Fizz" should === (FizzBuzz.start(6))
+  }
+
+  "A multiple of 5" should "return Buzz" in {
+    "Buzz" should === (FizzBuzz.start(10))
+  }
+
+  "A 15" should "return FizzBuzz" in {
+    "FizzBuzz" should === (FizzBuzz.start(15))
+  }
+
+  "A multiple of 3 and 5" should "return FizzBuzz" in {
+    "FizzBuzz" should === (FizzBuzz.start(45))
+  }
+
 }
